72a3048191d556b35e9a10d4c0de5029e63259f8
John Lewis <johnlewis@Lewis-Mac.local>
2017-08-05 14:50:20 -0400

    Parsing nmea data with pynmea2 package and showing lat longs

--- src/DataReceiver/NMEAListener.py
+++ src/DataReceiver/NMEAListener.py
@@ -27,15 +27,31 @@ for line in sys.stdin:
     dateFormatted = time.strftime('%m-%d-%Y', timeObj)
     timeFormatted = time.strftime('%I:%M:%S', timeObj)
 
-    print (dateFormatted + ' ' + timeFormatted + ':' + str(millisOnly))
 
     # This is like 61 of 6D or something...
     someStrangeSixHexTHing = timeTokens[1]
 
     # This is the nmea data we can parse
     nmeaStr = tokens[1]
+    #print (nmeaStr)
+
+    if not nmeaStr:
+        print ('nmeaStr is NULL.   Skipping')
+        continue
+    try:
+        nmeaObj = pynmea2.parse(nmeaStr)
+    except:
+        print ('nmeaStr is INVALID.   Skipping')
+        continue
+
+    if (nmeaObj.sentence_type == 'GGA'):
+        i = i + 1
+        print ('record from timstamp ' + dateFormatted + ' ' + timeFormatted + ':' + str(millisOnly))
+        print ('nmea data: ' + str(nmeaObj))
+        print ('lat  : ' + nmeaObj.lat)
+        print ('long : ' + nmeaObj.lon)
+        print ()
 
     # just output 5 lines and then stop for now.
-    i = i + 1
     if i > 5 :
         break;
